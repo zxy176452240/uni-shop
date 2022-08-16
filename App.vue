18eeee194da563f694313ba8b93d8b534071f4d6
zxy1764572240 <1764572240@qq.com>
2022-08-16 22:46:42 +0800

    tabBar

--- App.vue
+++ App.vue
@@ -15,10 +15,9 @@
 
 <style lang="scss">
 	/*每个页面公共css */
-	@import '@/uni_modules/uni-scss/index.scss';
-	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
-	// 设置整个项目的背景色
+	// @import '@/uni_modules/uni-scss/index.scss';
+	// /* #ifndef APP-NVUE */
+	// // 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
 	}
